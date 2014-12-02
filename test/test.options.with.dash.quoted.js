8dfd1fb44a01f32b7d3b4b14e04a47af4f9ff6b5
Henry Tseng <henryiis@gmail.com>
2014-12-02 11:11:41 -0500

    Corrected missing semi colon

--- test/test.options.with.dash.quoted.js
+++ test/test.options.with.dash.quoted.js
@@ -12,7 +12,7 @@ function parseRange(str) {
 program
   .version('0.0.1')
   .option('-d, --donuts [num]', -10)
-  .option('-a, --eater <person>', 'Sally Sue')
+  .option('-a, --eater <person>', 'Sally Sue');
 
 program.parse('node test -d "-8,2,2.32" -a Richard'.split(' '));
 program.donuts.should.equal('-8,2,2.32');
